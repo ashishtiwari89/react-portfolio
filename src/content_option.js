1bd290ac99678f32d5cf71dd5881834fe7b1e801
Ashish Tiwari <56654663+ashishtiwari89@users.noreply.github.com>
2023-02-17 12:55:21 -0800

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -7,9 +7,9 @@ const meta = {
 const introdata = {
     title: "I’m Ashish Tiwari",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "I am a Website Developer & Software Engineer",
+        second: "I love to develop beautiful websites",
+        third: "I am a Frontend Developer with 10 years of work experience",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
